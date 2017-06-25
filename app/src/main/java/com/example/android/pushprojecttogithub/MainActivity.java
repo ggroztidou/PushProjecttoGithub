af0032b9046b4e7712c7110392a18b00904bccd2
ggroztidou <groztidougeo@gmail.ocm>
2017-06-25 13:15:39 +0300

    Initial commit

---
+++ app/src/main/java/com/example/android/pushprojecttogithub/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.android.pushprojecttogithub;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    private void TestPush(){
+        int i=0;
+
+        i=i+1;
+    }
+}
